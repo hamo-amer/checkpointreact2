4e97329a988577c9b3691851e158044d3ee5007f
Mohamed <mohamedtaher.benarbia@gmail.com>
2020-07-18 19:47:15 +0200

    checkpoint-react2

---
+++ second_try/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import  Formul  from './Components/Formul';
+import Navb from './Components/Navb';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Navb></Navb>
+    <Formul></Formul>
+    </div>
+  );
+}
+
+export default App;
